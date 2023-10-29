144fc22dc43ad611f7f968723dd3ec13a47baf0a
Berkay Tuncel <berkaytncl98@gmail.com>
2023-10-29 22:30:25 +0300

    Add notes and examples for the Accessibility: Voice Over

---
+++ SwiftUIContinuedLearning/Contents/AccessibilityVoiceOverBootcamp.swift
@@ -0,0 +1,105 @@
+//
+//  AccessibilityVoiceOverBootcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Berkay Tuncel on 29.10.2023.
+//
+
+import SwiftUI
+
+struct AccessibilityVoiceOverBootcamp: View {
+    
+    @State private var isActive: Bool = false
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    Toggle("Volume", isOn: $isActive)
+                    
+                    HStack {
+                        Text("Volume")
+                        Spacer()
+                        
+                        Text(isActive ? "TRUE" : "FALSE")
+                            .accessibilityHidden(true)
+                    }
+                    .background(Color.black.opacity(0.001))
+                    .onTapGesture {
+                        isActive.toggle()
+                    }
+                    .accessibilityElement(children: .combine)
+                    .accessibilityAddTraits(.isButton)
+                    .accessibilityValue(isActive ? "is on" : "is off")
+                    .accessibilityHint("Double tap to toggle setting.")
+                    .accessibilityAction {
+                        isActive.toggle()
+                    }
+                } header: {
+                    Text("PREFERENCES")
+                }
+                
+                Section {
+                    Button("Favorites") {
+                        
+                    }
+                    .accessibilityRemoveTraits(.isButton)
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "heart.fill")
+                    })
+                    .accessibilityLabel("Favorites")
+                    
+                    Text("Favorites")
+                        .accessibilityAddTraits(.isButton)
+                        .onTapGesture {
+                            
+                        }
+                } header: {
+                    Text("APPLICATION")
+                }
+                
+                VStack {
+                    Text("CONTENT")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundStyle(.secondary)
+                        .font(.caption)
+                        .accessibilityAddTraits(.isHeader)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 8) {
+                            ForEach(0 ..< 10) { x in
+                                VStack {
+                                    Image(.steveJobs)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 100, height: 100)
+                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                    
+                                    Text("Item \(x)")
+                                }
+                                .onTapGesture {
+                                    
+                                }
+                                .accessibilityElement(children: .combine)
+                                .accessibilityAddTraits(.isButton)
+                                .accessibilityLabel("Item \(x). Image of Steve Jobs.")
+                                .accessibilityHint("Double tap to open.")
+                                .accessibilityAction {
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Setting")
+        }
+    }
+}
+
+#Preview {
+    AccessibilityVoiceOverBootcamp()
+}
